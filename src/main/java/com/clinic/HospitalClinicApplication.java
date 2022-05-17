4c3f42901d73211a9dedf8ac109f8dece5d67677
DESKTOP-0KS6LGU\marco <marcorossi.uni.inf@gmail.com>
2022-05-17 13:32:14 +0200

    initial commit

---
+++ src/main/java/com/clinic/HospitalClinicApplication.java
@@ -0,0 +1,13 @@
+package com.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalClinicApplication.class, args);
+	}
+
+}

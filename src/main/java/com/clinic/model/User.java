19969545d02f95dd8a63e7bd0fc203ecc0ee4318
DESKTOP-LPAFG9R\Tri <tristanpgeronimo@gmail.com>
2022-05-27 11:40:37 +0200

    create - User.java (model)

---
+++ src/main/java/com/clinic/model/User.java
@@ -0,0 +1,81 @@
+package com.clinic.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User{
+
+    private int user_id;
+    private Patient patient_id;
+    private Doctor doctor_id;
+
+    public User_kind user_kind;
+    private String username;
+    private String password;
+
+    public User(int user_id, Patient patient_id, Doctor doctor_id, String username, String password) {
+        this.user_id = user_id;
+        this.patient_id = patient_id;
+        this.doctor_id = doctor_id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    @ManyToOne
+    public Patient getPatient_id() {
+        return patient_id;
+    }
+
+    public void setPatient_id(Patient patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    @ManyToOne
+    public Doctor getDoctor_id() {
+        return doctor_id;
+    }
+
+    public void setDoctor_id(Doctor doctor_id) {
+        this.doctor_id = doctor_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
+
+enum User_kind {
+    PATIENT,
+    DOCTORS,
+    SECRETARY;
+}
+

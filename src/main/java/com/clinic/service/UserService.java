87956c0f6b665e0a87743c2805f69cc1b6fdf768
DESKTOP-LPAFG9R\Tri <tristanpgeronimo@gmail.com>
2022-05-30 12:17:03 +0200

    created - USER ENTITY (entity, controller, repo, service)

---
+++ src/main/java/com/clinic/service/UserService.java
@@ -0,0 +1,33 @@
+package com.clinic.service;
+
+import com.clinic.model.User;
+import com.clinic.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> listAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    public void deleteUsers(Integer id) {
+        userRepository.deleteById(id);
+    }
+}

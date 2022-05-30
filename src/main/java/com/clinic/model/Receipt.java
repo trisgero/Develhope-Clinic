91d0e2f479bc00c0907aa1c0aa0119549fc14b35
Sonia Curridori <soniacurridori@gmail.com>
2022-05-30 13:59:16 +0200

    addition of FK patient_id and doctor_id

--- src/main/java/com/clinic/model/Receipt.java
+++ src/main/java/com/clinic/model/Receipt.java
@@ -1,6 +1,7 @@
 package com.clinic.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * aggiungere patient_id e doctor_id
@@ -9,11 +10,42 @@ import javax.persistence.*;
 @Entity
 @Table(name = "receipt")
 public class Receipt {
+
     @Id
     @Column(name = "id", nullable = false)
     private Long receipt_id;
     private String receipt_description;
+    private List<Patient> patient_id;
 
+    public Long getReceipt_id() {
+        return receipt_id;
+    }
+
+    public void setReceipt_id(Long receipt_id) {
+        this.receipt_id = receipt_id;
+    }
+
+    @OneToMany
+    public List<Patient> getPatient_id() {
+        return patient_id;
+    }
+
+    @OneToMany
+    public void setPatient_id(List<Patient> patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    @OneToMany
+    public List<Doctor> getDoctor_id() {
+        return doctor_id;
+    }
+
+    @OneToMany
+    public void setDoctor_id(List<Doctor> doctor_id) {
+        this.doctor_id = doctor_id;
+    }
+
+    private List<Doctor> doctor_id;
     public Long getId() {
         return receipt_id;
     }
